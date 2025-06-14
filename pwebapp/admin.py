730d8c64b03443d67fd522f3f4798d72142e90fd
Marcelo Rodrigues <marcelorj280@gmail.com>
2025-06-14 20:54:10 -0300

    feat(admin): register models with custom admin classes for better management

--- pwebapp/admin.py
+++ pwebapp/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
-from .models import User, Person, Product, Order, OrderItem
+from .models import Product, Order, OrderItem
 
-admin.site.register(User)
-admin.site.register(Person)
-admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(OrderItem)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+    search_fields = ('name',)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    exclude = ('closed_at',)
+    list_display = ('table_number', 'responsible_name', 'is_paid', 'opened_at', 'closed_at')
+    list_filter = ('is_paid',)
+    search_fields = ('table_number', 'responsible_name')
+    readonly_fields = ('opened_at',)
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'product', 'quantity')
+    list_filter = ('product',)
